b986ff0487663c64e35a016fb4a4f14cf140c23e
emin bayrak <eminb@outlook.com>
2018-06-03 09:48:49 -0400

    update

--- ParPorApp/ParPorApp/Constants.cs
+++ ParPorApp/ParPorApp/Constants.cs
@@ -6,15 +6,12 @@
         //public static string BaseApiAddress => "http://parentportalapi.azurewebsites.net/";
 
         //Home
-        //public static string BaseApiAddress => "http://192.168.29.110:5000/";
+        public static string BaseApiAddress => "http://192.168.148.2:5000/";
 
         //Work
-        public static string BaseApiAddress => "http://10.65.234.100:5000/";
+        //public static string BaseApiAddress => "http://10.65.234.100:5000/";
 
-        //schools
-        //public static string BaseApiAddress => "http://10.90.229.234:5000/";
-
-		//Localhost
+        //Localhost
 	    //public static string BaseApiAddress => "http://localhost:55601/";
 
 		//public static int LogoIconHeight = 120;
